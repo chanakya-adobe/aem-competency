b1fd40aaea0983a4a47584c8b85f5124b5ff66b6
Mohit Kumar <mohitkumar@adobe.com>
2024-05-09 19:04:51 +0530

    reverting to latest boilerplate

--- blocks/image-slider/image-slider.js
+++
@@ -1,9 +0,0 @@
-export default async function decorate(block) {
-  const parent = block.querySelector('picture').parentElement;
-  const pictures = parent.querySelectorAll('picture');
-  // duplicate pictures to create a slideshow
-  pictures.forEach((picture) => {
-    const clone = picture.cloneNode(true);
-    parent.appendChild(clone);
-  });
-}
--- blocks/picture/picture.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    row.children[1].classList.add(row.children[0].innerText);
-    block.replaceChild(row.children[1], row);
-  });
-}
