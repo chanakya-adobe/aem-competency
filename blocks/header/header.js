e6e759b3d6d5a372f59452e73b47e29fb1506ce7
Abhishek301983 <153721492+Abhishek301983@users.noreply.github.com>
2024-05-14 12:28:39 +0530

    Updates for Desktop view

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -122,7 +122,7 @@ export default async function decorate(block) {
   if (navSections) {
     navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
       if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-      navSection.addEventListener('mouseover', () => {
+      navSection.addEventListener('click', () => {
         if (isDesktop.matches) {
           const expanded = navSection.getAttribute('aria-expanded') === 'true';
           toggleAllNavSections(navSections);
