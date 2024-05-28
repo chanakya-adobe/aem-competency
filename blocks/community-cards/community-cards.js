f253bf24a416fd970ae5bde2165abb9b63b0be3f
Akash Gupta <akasgupta@adobe.com>
2024-05-28 21:35:30 +0530

    fixed lint issues

--- blocks/community-cards/community-cards.js
+++ blocks/community-cards/community-cards.js
@@ -1,24 +1,40 @@
 import {
   fetchSearch, CATEGORY_BIGBETS, CATEGORY_FORUM, CATEGORY_MENTORING,
 } from '../../scripts/scripts.js';
-import { readBlockConfig } from '../../scripts/aem.js';
 import { getTagList } from '../../scripts/utils.js';
+import { fetchPlaceholders, toCamelCase } from '../../scripts/aem.js';
 
-async function getCommunityCards(communityList, block) {
-  const blockConfig = readBlockConfig(block);
-  const sortedCardItems = Object.keys(blockConfig).sort().map((key) => blockConfig[key]);
-  const arr = Array.from(Array(sortedCardItems.length));
-  communityList.filter((item) => sortedCardItems.includes(item.path)).forEach((item) => {
-    const position = sortedCardItems.indexOf(item.path);
-    arr[position] = item;
+async function getCommunityCards(communityList, itemsPath) {
+  const arr = [];
+  itemsPath.forEach((item) => {
+    // filter if the itemsPath has valid category path
+    const cardItem = communityList.filter((listItem) => listItem.path === item)
+      .map((listItem) => listItem);
+    if (cardItem[0]) {
+      arr.push(cardItem[0]);
+    }
   });
-  return arr;
+
+  // not more than 3 cards are allowed
+  return arr.length >= 3 ? arr.splice(0, 3) : arr.splice(0, arr.length);
 }
 
-const generateBigBetsCard = (card) => {
+const metaAuthorImgHTML = (card, authorImg) => `<div class="owner card-meta-items">Owner: <img src="${authorImg}" title="${card.author}" width="24" height="24" /> <strong>${card.author}</strong><span class="meta-separator">|</span></div>`;
+const metaAuthorHTML = (card) => `<div class="owner card-meta-items">Owner:&nbsp;<strong> ${card.author}</strong><span class="meta-separator">|</span></div>`;
+
+const getAuthorImage = (card, placeholder) => {
+  const authorImage = placeholder[toCamelCase(`user-${card.author}`)];
+  if (authorImage) {
+    return metaAuthorImgHTML(card, authorImage);
+  }
+  return metaAuthorHTML(card, authorImage);
+};
+
+const generateBigBetsCard = (card, placeholder) => {
   const bigBetsContainer = document.createElement('div');
   bigBetsContainer.classList.add('card');
   bigBetsContainer.classList.add('big-bets-content');
+  const authorImage = getAuthorImage(card, placeholder);
   const bigBetContent = `
       <div class="card-header">
       <h3>${card.title}</h3>
@@ -27,8 +43,10 @@ const generateBigBetsCard = (card) => {
       <p class="card-description">${card.description}</p>
       <div class="card-meta">
       <div class="card-meta-first-section">
-        <div class="visibility card-meta-items icon-container"><span class="icon icon-globe"><img data-icon-name="globe" src="/icons/globe.svg" alt="" loading="lazy"></span>${card.visibility}</div>
-        <div class="owner card-meta-items">Owner <img src="https://main--aem-competency--aem-comp.hlx.live/assets/users/media_15e8d069f911ce3c77611de6ae5818e6445deaf30.png"></img><strong>${card.author}</strong></div>
+        <div class="visibility card-meta-items icon-container"><span class="icon icon-globe"><img data-icon-name="globe" src="/icons/globe.svg" alt="" loading="lazy"></span>${card.visibility}
+          <span class="meta-separator">|</span>
+        </div>
+        ${authorImage}
       </div>  
         <div class="status">Status: <strong>${card.status}</strong></div>
       </div>`;
@@ -43,32 +61,55 @@ const generateBigBetsCard = (card) => {
 };
 
 const generateForumCard = (card) => {
-  
+  // TODO: update the logic for rendering forum card
+  generateBigBetsCard(card);
 };
 
 const generateMentoringCard = (card) => {
-
+  // TODO: update the logic for rendering forum card
+  generateBigBetsCard(card);
 };
 
-const getCardListHtml = (cards, cardContainer) => {
+const getCardListHtml = (cards, cardContainer, placeholder) => {
   while (cards.length > 0) {
     const card = cards.splice(0, 1)[0];
     switch (card.category) {
-      case CATEGORY_BIGBETS: cardContainer.append(generateBigBetsCard(card)); break;
-      case CATEGORY_FORUM: cardContainer.append(generateForumCard(card)); break;
-      case CATEGORY_MENTORING: cardContainer.append(generateMentoringCard(card)); break;
+      case CATEGORY_BIGBETS: cardContainer.append(generateBigBetsCard(card, placeholder)); break;
+      case CATEGORY_FORUM: cardContainer.append(generateForumCard(card, placeholder)); break;
+      case CATEGORY_MENTORING: cardContainer.append(generateMentoringCard(card, placeholder));
+        break;
       default:
     }
   }
 };
 
+const getCommunityCardItemsPath = (block) => {
+  const cardItemsPath = [];
+  block.querySelectorAll(':scope > div').forEach((row) => {
+    // checking if row is having child div
+    if (row.children) {
+      const cols = [...row.children];
+      if (cols && cols.length > 0) {
+        const col = cols[0];
+        const itemPath = col.querySelector('p')?.textContent;
+        if (itemPath) {
+          cardItemsPath.push(itemPath);
+        }
+      }
+    }
+  });
+  return cardItemsPath;
+};
+
 export default async function decorate(block) {
-  const communityList = await fetchSearch();
-  const cards = await getCommunityCards(communityList, block);
+  const itemsPath = getCommunityCardItemsPath(block);
   block.textContent = '';
+  const communityList = await fetchSearch();
+  const cards = await getCommunityCards(communityList, itemsPath);
   const cardContainer = document.createElement('div');
   cardContainer.className = 'cc-container';
-  getCardListHtml(cards, cardContainer);
+  const placeholder = await fetchPlaceholders();
+  getCardListHtml(cards, cardContainer, placeholder);
 
   block.append(cardContainer);
 }
