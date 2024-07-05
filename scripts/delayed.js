d81939d9883fef86785ba8f706def85e596823af
Shashank Singh <shashanksing@adobe.com>
2024-07-05 13:07:24 +0530

    Fixing lint issues

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,9 +5,9 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 function externalLinks() {
-  for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) {
-    var b = c[a];
-    b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+    let b = c[a];
+    b.getAttribute('href') && b.hostname !== location.hostname && (b.target = '_blank')
   }
 }
 
