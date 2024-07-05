f37db8a44a1d71e0641108a79c89dd7f6cfe0997
Mohit Kumar <mohitkumar@adobe.com>
2024-07-05 19:06:49 +0530

    optimized logic by moving it in modal loop

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,17 +3,3 @@ import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
-function externalLinks() {
-  const links = document.getElementsByTagName('a');
-  let index = 0;
-  while (index < links.length) {
-    const redirect = links[index];
-    if (redirect.getAttribute('href') && redirect.hostname !== window.location.hostname) {
-      redirect.target = '_blank';
-    }
-    index += 1;
-  }
-}
-
-externalLinks();
